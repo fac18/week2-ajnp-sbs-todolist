2bef5b8739925e86bda183ce011be2b2b37b53ce
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2019-11-06 16:19:57 +0000

    building tests for addtodo function
    Co-authored-by: kin-au <kinnamau@gmail.com>

--- test.js
+++ test.js
@@ -1,7 +1,76 @@
-var test = require('tape');
-var logic = require('./logic');
+var test = require("tape");
+var todoFunctions = require("./logic");
+var twoTodos = [
+  {
+    id: 0,
+    description: "smash avocados",
+    done: true
+  },
+  {
+    id: 1,
+    description: "make coffee",
+    done: false
+  }
+];
+var threeTodos = [
+  {
+    id: 0,
+    description: "smash avocados",
+    done: true
+  },
+  {
+    id: 1,
+    description: "make coffee",
+    done: false
+  },
+  {
+    id: 2,
+    description: "drink",
+    done: false
+  }
+];
 
-test('Example test', function(t) {
+test("Example test", function(t) {
   t.pass();
   t.end();
 });
+
+// should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
+test("Input argument todos has not been altered in addTodo function", function(t) {
+  t.equal(
+    twoTodos,
+    [
+      {
+        id: 0,
+        description: "smash avocados",
+        done: true
+      },
+      {
+        id: 1,
+        description: "make coffee",
+        done: false
+      }
+    ],
+    "todos argument has not been altered."
+  );
+  t.end();
+});
+
+test("returns array with original todos and the newTodo added to the end in addTodo function", function(t) {
+  t.equal(
+    todoFunctions.addTodo(twoTodos, "drink"),
+    threeTodos,
+    "Should return array containing todos with the newTodo added to the end."
+  );
+  t.end();
+});
+
+// test("newTodo has an id key in addTodo function", function(t) {
+//   var actual
+//   var expected
+//
+//   t.equal(
+//
+//   );
+//   t.end();
+// });
